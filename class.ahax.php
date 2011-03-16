1dc842a71c00c61b86d55bb5c00528477a49d181
Daniel Doezema <dan@doezema.com>
2011-03-15 23:31:06 -0400

    Reworked the logic of how the 'action' property is used when hooking into a specific ahax request.

--- class.ahax.php
+++ class.ahax.php
@@ -7,22 +7,31 @@
 */
 class Ahax {
     
-    // The filter tag used to hook into the ajax request.
-    const FILTER_TAG = 'ahax_request';
+    /**
+    * The filter tag used to hook into the ajax request.
+    */
+    const PREFIX = 'ahax_request_';
     
-    // Allow the output of errors.
-    protected $debug = true;
+    /**
+    * Enable / Disable output of errors.
+    */
+    protected $is_debugging = false;
     
-    public function __construct() {
-        
-    }
+   
+    /**
+    * Create a ahax tag that can be hooked into.
+    */
+    public static function tag($action) {
+        return self::PREFIX . $action;
+    } 
 
-    // Handle the ajax requst
+    /**
+    * Handle the AJAX request.
+    */
     public function handleRequest() {
         if($this->isCalledFromCorrectContext()) {
             if(is_string($action = $this->getAction())) {
-                $output = '';
-                $output = apply_filters(self::FILTER_TAG, $output, $action);
+                $output = apply_filters(self::tag($action), $output = '', $this);
                 $this->sendOutput($output);
             }
             $this->sendError('Invalid Action');            
@@ -30,7 +39,10 @@ class Ahax {
         $this->sendError('Invalid Context');
     }
 
-    // Return the action from $_POST['action'] > $_GET['action']
+    /**
+    * Return the action from either the $_POST or $_GET
+    * $_POST overrides $_GET
+    */
     protected function getAction() {
         $action = false;
         $params = array_map('trim', array_merge($_GET, $_POST));
@@ -42,28 +54,37 @@ class Ahax {
         return $action;
     }
     
-    // Send filtered output to the output buffer and die.
+    /**
+    * Send a string to the output buffer, then die.
+    */
     protected function sendOutput($output) {
         echo $output;
         die;
     }
 
-    // Send an error to the output buffer and die (send = output & die).
+    /**
+    * Send an error string to the output buffer, then die.
+    */
     protected function sendError($output = -1) {
         if($this->isDebugging()) {
-            echo $output;
+            echo 'Error: '. $output;
         }
         die;
     }
 
+    /**
+    * Get / Set is debugging is enabled.
+    */
     protected function isDebugging($bool = null) {
         if(is_bool($bool)) {
-            $this->debug = $bool;
+            $this->is_debugging = $bool;
         }
-        return $this->debug;
+        return $this->is_debugging;
     } 
 
-    // Make sure the request has been made from the ahax/request.php file.
+    /**
+    * Make sure the request has been made from the ahax/request.php file.
+    */
     protected function isCalledFromCorrectContext() {
         if(stripos($_SERVER['REQUEST_URI'], 'wp-content/plugins/ahax/request.php') !== false) {
             return true;
