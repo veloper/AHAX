5478c31e834fbe2c75c056806c7de6c5a9bce5b3
Daniel Doezema <dan@doezema.com>
2011-04-14 02:22:25 -0400

    Added WordPress wp_localize_script() call for passing the absolute path to the request.php file.

--- actions.php
+++ actions.php
@@ -5,4 +5,5 @@
 add_action('wp_head', 'ahax_add_js', 1);
 function ahax_add_js() {
     wp_enqueue_script( 'ahaxJs', get_bloginfo('siteurl') . '/wp-content/plugins/ahax/ahax.js');
+	wp_localize_script( 'ahaxJs', 'AHAXConfig', array( 'url' => get_bloginfo('siteurl') . '/wp-content/plugins/ahax/request.php' ) );
 }
